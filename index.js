623b13d9c02784d878b710a80d90eeaa0b149884
Nielleeeee <jandnllplz@gmail.com>
2024-12-20 00:38:49 +0800

    refactor: added try catch on async codes

--- index.js
+++ index.js
@@ -95,6 +95,61 @@ app.delete("/users/:id", (req, res) => {
   return res.status(200).json(user);
 });
 
+// File checker
+app.post("/file/check", upload.single("file"), (req, res) => {
+  try {
+    const file = req.file;
+
+    if (!file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+
+    if (file.mimetype !== "text/plain") {
+      return res.status(400).json({ message: "Invalid file type" });
+    }
+
+    const fileContent = file.buffer.toString("utf-8");
+    const letterCount = fileContent.length;
+    const wordCount = fileContent.split(/\s+/).filter(Boolean).length;
+
+    return res
+      .status(200)
+      .json({ message: "File checked successfully", wordCount, letterCount });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error", error });
+  }
+});
+
+// Fetch sample data
+app.get("/fetch", async (req, res) => {
+  try {
+    const rawData = await fetch(
+      "https://64f4a6b7932537f4051a9111.mockapi.io/nielle/store-data"
+    );
+    const data = await rawData.json();
+
+    return res.status(200).json({ message: "Fetching data from API", data });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// console.log('Start of the program');
+
+// fs.readFile(__filename, () => {
+//   console.log('I/O callback: File read completed');
+// });
+
+// setTimeout(() => {
+//   console.log('setTimeout: Timer phase');
+// }, 1000);
+
+// setImmediate(() => {
+//   console.log('setImmediate: Check phase');
+// });
+
+// console.log('End of the program');
