3f6274397f2fbbe35e44a126cd67dc3cb0dee38c
Nielleeeee <jandnllplz@gmail.com>
2024-12-21 00:26:19 +0800

    feat: created pool connection for postgresql db

---
+++ db.js
@@ -0,0 +1,21 @@
+const { Pool } = require("pg");
+
+require("dotenv").confgig();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+pool.on("error", (err) => {
+  console.error("Error in database connection", err);
+});
+
+pool.on("connect", () => {
+  console.log("Connected to PostgreSQL");
+});
+
+module.exports = pool;
