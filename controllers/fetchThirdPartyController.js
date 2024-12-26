8cd68310cda3216c587e1f2016ae31fdd5057b1a
Nielleeeee <jandnllplz@gmail.com>
2024-12-27 01:33:14 +0800

    chore: created fetch third party controller

---
+++ controllers/fetchThirdPartyController.js
@@ -0,0 +1,12 @@
+export const fetchThirdPartyData = async (req, res) => {
+  try {
+    const rawData = await fetch(
+      "https://64f4a6b7932537f4051a9111.mockapi.io/nielle/store-data"
+    );
+    const data = await rawData.json();
+
+    return res.status(200).json({ message: "Fetching data from API", data });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
