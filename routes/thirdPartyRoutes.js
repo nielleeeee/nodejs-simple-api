70675c032f5f943f154e21ddf1275fd68e2000f2
Nielleeeee <jandnllplz@gmail.com>
2024-12-30 00:40:15 +0800

    feat: move to src folder

--- routes/thirdPartyRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { fetchThirdPartyData } from "../controllers/fetchThirdPartyController.js";
-
-const router = express.Router();
-
-router.post("/", fetchThirdPartyData);
-
-export default router;
