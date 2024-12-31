8705d1d9c216e97b7b0a672ea64c8f0e05b3d8b2
Nielleeeee <jandnllplz@gmail.com>
2025-01-01 06:37:52 +0800

    chore: added ignore patterns

--- eslint.config.js
+++ eslint.config.js
@@ -26,11 +26,13 @@ export default [
 
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-argument": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": [
         "error",
         { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
     },
+    ignorePatterns: ["dist", "node_modules"],
   },
 ];
