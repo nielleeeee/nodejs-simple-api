87f12c4a679018a0fdacda474bf4b8a01e0afb9c
Nielleeeee <jandnllplz@gmail.com>
2025-01-29 02:15:32 +0800

    chore: removed db migration file

--- src/db/migrations/001-create-todos-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS todos (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    completed BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
